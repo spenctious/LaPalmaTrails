0f847087cd041258e60e8546549a7b1a765c9aaa
Rob Anstey <robert.anstey@gmail.com>
2022-12-01 16:12:06 +0000

    Added test for recognising correct GR trails

---
+++ LaPalmaTrailsAPI.Tests/MockWebReader.cs
@@ -0,0 +1,29 @@
+﻿using LaPalmaTrailsAPI;
+
+namespace LaPalmaTrailsAPI.Tests
+{
+    public class MockWebReader : IWebReader
+    {
+        TimeSpan _timeout = TimeSpan.FromMilliseconds(1000);
+
+        public async Task<string> GetStringAsync(string? testPage)
+        {
+            string testDataSource = @"C:\Users\Robert\projects\LaPalmaTrailsAPI\LaPalmaTrailsAPI.Tests\TestData\";
+            return await File.ReadAllTextAsync(testDataSource + (testPage ?? ""));
+        }
+
+        public TimeSpan Timeout
+        {
+            get
+            {
+                return _timeout;
+            }
+
+            set
+            {
+                _timeout = value;
+            }
+        }
+    }
+}
+
--- LaPalmaTrailsAPI.Tests/UnitTest1.cs
+++ LaPalmaTrailsAPI.Tests/UnitTest1.cs
@@ -1,11 +1,28 @@
 namespace LaPalmaTrailsAPI.Tests
 {
-    public class UnitTest1
+    public class StatusScraperTests
     {
         [Fact]
-        public void Test1()
+        public async Task Valid_GR_paths_recognised()
         {
-            Assert.True(true);
+            StatusScraper sut = new();
+            sut.StatusPage = "Valid_GR_trails.html";
+
+            var scraperResult = await sut.GetTrailStatuses(new MockWebReader());
+
+            Assert.Equal(ScraperEvent.EventType.Success.ToString(), scraperResult.Result.Type);
+            Assert.Equal(2, scraperResult.Trails.Count);
+            Assert.Empty(scraperResult.Anomalies);
+
+            // N.B. scraper results are listed in reverse order to their appearance in the test file
+
+            Assert.Equal("Open",                            scraperResult.Trails[0].Status);
+            Assert.Equal("Link_to_English_version.html",    scraperResult.Trails[0].Url);
+            Assert.Equal("GR 131 Etapa 1",                  scraperResult.Trails[0].Name);
+
+            Assert.Equal("Open",                            scraperResult.Trails[1].Status);
+            Assert.Equal("Link_to_English_version.html",    scraperResult.Trails[1].Url);
+            Assert.Equal("GR 130 Etapa 1",                  scraperResult.Trails[1].Name);
         }
     }
 }
