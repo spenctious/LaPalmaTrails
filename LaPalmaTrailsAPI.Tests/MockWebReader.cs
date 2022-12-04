01af319a0d580507078a1460492adb9d90137ecf
Rob Anstey <robert.anstey@gmail.com>
2022-12-04 21:49:51 +0000

    Modified to allow different exceptions to be thrown for different calls

--- LaPalmaTrailsAPI.Tests/MockWebReader.cs
+++ LaPalmaTrailsAPI.Tests/MockWebReader.cs
@@ -5,12 +5,17 @@ namespace LaPalmaTrailsAPI.Tests
     public class MockWebReader : IWebReader
     {
         TimeSpan _timeout = TimeSpan.FromMilliseconds(1000);
-        public Exception? SimulatedException { get; set; } = null;
+
+        public Dictionary<string, Exception> SimulateException = new();
 
         public async Task<string> GetStringAsync(string? testPage)
         {
             // simulate a timeout
-            if (SimulatedException != null) throw SimulatedException;
+            Exception exception;
+            if (SimulateException.TryGetValue(testPage, out exception))
+            {
+                throw exception;
+            }
 
             string testDataSource = @"C:\Users\Robert\projects\LaPalmaTrailsAPI\LaPalmaTrailsAPI.Tests\TestData\";
             return await File.ReadAllTextAsync(testDataSource + (testPage ?? ""));
