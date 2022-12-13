409e4d1ed000883f4a680b02801afba50e83afba
Rob Anstey <robert.anstey@gmail.com>
2022-12-13 21:43:17 +0000

    Updated for CachedUrlLookupTable

--- LaPalmaTrailsAPI.Tests/UrlLookupTableTests.cs
+++ LaPalmaTrailsAPI.Tests/UrlLookupTableTests.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using FluentAssertions;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
@@ -11,138 +12,71 @@ namespace LaPalmaTrailsAPI.Tests
     [ExcludeFromCodeCoverage]
     public class UrlLookupTableTests
     {
-        private void DeleteLookupTableFile()
-        {
-            if (File.Exists(PersistentLookupTable.UrlTableLookupFileName))
-            {
-                File.Delete(PersistentLookupTable.UrlTableLookupFileName);
-
-            }
-        }
-
-        private void CreateLookupTableFile(string contents)
-        {
-            File.WriteAllText(PersistentLookupTable.UrlTableLookupFileName, contents);
-        }
-
-
         [Fact]
         public void Missing_file_returns_false()
         {
-            DeleteLookupTableFile();
-            var sut = new PersistentLookupTable();
+            // Arrange
+            TestHelper.DeleteLookupTableFile();
 
-            bool loaded = sut.LoadUrlLookupTable();
+            // Act
+            bool loaded = CachedUrlLookupTable.Instance.LoadFromFile();
 
-            Assert.False(loaded);
+            // Assert
+            loaded.Should().Be(false);
         }
 
 
         [Fact]
         public void Exisitng_file_is_loaded_returns_true()
         {
-            CreateLookupTableFile(@"{ ""Spanish"": ""English"" }");
-            var sut = new PersistentLookupTable();
+            // Arrange
+            TestHelper.CreateLookupTableFile(@"{ ""Spanish"": ""English"" }");
 
-            bool loaded = sut.LoadUrlLookupTable();
+            // Act
+            bool loaded = CachedUrlLookupTable.Instance.LoadFromFile();
 
-            Assert.True(loaded);
-            Assert.Equal(1, sut.Count);
+            // Assert
+            loaded.Should().Be(true);
+            CachedUrlLookupTable.Instance.Value.Count.Should().Be(1);
         }
 
 
         [Fact]
         public void Saving_table_creates_file()
         {
-            DeleteLookupTableFile();
-            var sut = new PersistentLookupTable();
-            sut.TryAdd("Spanish", "English");
+            // Arrange
+            TestHelper.DeleteLookupTableFile();
+            CachedUrlLookupTable.Instance.Value.Clear();
 
-            sut.SaveUrlLookupTable();
+            // Act
+            bool added = CachedUrlLookupTable.Instance.Value.TryAdd("Spanish", "English");
+            CachedUrlLookupTable.Instance.SaveToFile();
 
-            Assert.True(File.Exists(PersistentLookupTable.UrlTableLookupFileName));
+            // Assert
+            added.Should().BeTrue();
+            File.Exists(CachedUrlLookupTable.UrlTableLookupFileName).Should().BeTrue();
         }
 
 
         [Fact]
         public void Saving_table_overwrites_existing_file()
         {
-            CreateLookupTableFile(@"{ ""Spanish"": ""German"" }");
-            var sut = new PersistentLookupTable();
-            sut.Clear();
-            sut.TryAdd("Spanish", "English");
+            // Arrange
+            TestHelper.CreateLookupTableFile(@"{ ""Spanish"": ""German"" }"); // file to be overwritten
+            CachedUrlLookupTable.Instance.Value.Clear();
+            CachedUrlLookupTable.Instance.Value.TryAdd("Spanish", "English");
 
-            sut.SaveUrlLookupTable();
-            sut.Clear();
-            sut.LoadUrlLookupTable();
+            // Act
+            CachedUrlLookupTable.Instance.SaveToFile();
+            CachedUrlLookupTable.Instance.Value.Clear();
+            CachedUrlLookupTable.Instance.LoadFromFile();
 
-            Assert.Equal(1, sut.Count);
-            Assert.Equal("English", sut.GetValue("Spanish"));
-        }
-
-
-        [Fact]
-        public void New_url_association_is_added()
-        {
-            var sut = new PersistentLookupTable();
-            sut.Clear();
-
-            sut.TryAdd("Spanish", "English");
-
-            Assert.Equal(1, sut.Count);
-            Assert.Equal("English", sut.GetValue("Spanish"));
-        }
-
-
-        [Fact]
-        public void Adding_existing_key_does_not_change_value()
-        {
-            var sut = new PersistentLookupTable();
-            sut.Clear();
-            sut.TryAdd("Spanish", "English");
-
-            sut.TryAdd("Spanish", "German");
-
-            Assert.Equal(1, sut.Count);
-            Assert.Equal("English", sut.GetValue("Spanish"));
-        }
-
-
-        [Fact]
-        public void Missing_key_returns_false()
-        {
-            var sut = new PersistentLookupTable();
-            sut.Clear();
-
-            bool itemLocated = sut.ContainsKey("Spanish");
-
-            Assert.False(itemLocated);
-        }
-
-
-        [Fact]
-        public void Found_key_returns_true()
-        {
-            var sut = new PersistentLookupTable();
-            sut.Clear();
-            sut.TryAdd("Spanish", "English");
-
-            bool itemLocated = sut.ContainsKey("Spanish");
-
-            Assert.True(itemLocated);
-        }
-
-
-        [Fact]
-        public void Existing_association_returns_value()
-        {
-            var sut = new PersistentLookupTable();
-            sut.Clear();
-            sut.TryAdd("Spanish", "English");
-
-            string associatedValue = sut.GetValue("Spanish");
-
-            Assert.Equal("English", associatedValue);
+            // Assert
+            CachedUrlLookupTable.Instance.Value.Count.Should().Be(1);
+            string expectedUrl;
+            bool lookupSucceeded = CachedUrlLookupTable.Instance.Value.TryGetValue("Spanish", out expectedUrl);
+            lookupSucceeded.Should().BeTrue();
+            expectedUrl.Should().Be("English");
         }
     }
 }
